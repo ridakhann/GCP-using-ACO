12227897ff002c047d7e3c3521b17f353a994974
Rida Khan <49796698+ridakhann@users.noreply.github.com>
2021-03-20 03:42:41 +0500

    added documentation and highlighted the paper

--- question1.py
+++ question1.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.classes.function import nodes
 import numpy as np
+from numpy import random
 import random
 
 
@@ -18,13 +19,14 @@ class Ant:
         self.assigned_colors = {}
         self.colors_used = 0  # number of colours used in a solution
 
-    def solution_init(self, g, colors, start=None):
+    def solution_init(self, g, colors, start=None):  # constructing solutions for each ant
         self.available_colors = sorted(colors.copy())
         keys = [x for x in nodes]
         for key in keys:
             self.assigned_colors[key] = None
 
-        if start == None:
+        if start == None:  # if start not already assigned
+            # randomly assigning a starting node/position for the ant
             self.start = random.choice(nodes)
         else:
             self.start = start
@@ -33,56 +35,61 @@ class Ant:
         self.notvisited = nodes.copy()
 
         if len(self.visited) == 0:
+            # assigning the smallest possible colour to the vertex if it is the first step
             self.assign_color(self.start, self.available_colors[0])
         return self
 
-    def assign_color(self, node, color):
+    def assign_color(self, node, color):  # assigning colour to a specified node
         self.assigned_colors[node] = color
+        # adding node to the ant's tabu list so as to not go there again
         self.visited.append(node)
         self.notvisited.remove(node)
 
-    def color_graph(self):
-        tabu = []
+    def color_graph(self):  # construction method, colouring graph
+        tabu = []  # keep track of colours of neighbours to make sure not to colour the vertex same as its neighbor
         len_notvisited = len(self.notvisited)
         for i in range(len_notvisited):
+            # applying the transition rule to choose the next node to colour
             next = self.choose_next()
             tabu = []  # emptying tabu list
             for j in range(number_of_nodes):
+                # if neighbours add the colour of neighbour to tabu list
                 if(adjacency_mat[next, j] == 1):
                     tabu.append(self.assigned_colors[j])
-            for k in self.available_colors:
+            for k in self.available_colors:  # checking list of over all colours available
                 if (k not in tabu):
+                    # assigning colour a colour that has not been restricted by the tabu list
                     self.assign_color(next, k)
                     break
+            # updating the total number of colours used
             self.colors_used = len(set(self.assigned_colors.values()))
 
-    def diff_color(self, node=None): #desiribility
-        if node is None:
+    # calculating desiribility of a node based on the most number of conflicts a node has (degree of saturation)
+    def diff_color(self, node=None):
+        if node is None:  # case for the very first node
             node = self.start
-        neighb = []
+        neighb = []  # list of neighbors
         for i in range(number_of_nodes):
             if adjacency_mat[node, i] == 1:
                 neighb.append(self.assigned_colors[i])
-        return len(set(neighb)) #unique colours within neighbours
+        return len(set(neighb))  # unique colours within neighbours
 
-    def get_phero(self, node, neighbor):
+    def get_phero(self, node, neighbor):  # returns the amount of pheromone on an edge
         return phero_mat[node, neighbor]
 
-    def choose_next(self):  # check this later
+    def choose_next(self):  # transition method to choose next node to colour
         maxval = 0
         vals = []
         summation = 0
         candidates = []
         candidates_possible = []
         for j in self.notvisited:
-            vals.append((self.get_phero(self.start, j) ** self.alpha)
-                        * (self.diff_color(j)**self.beta))
+            vals.append(self.get_phero(self.start, j)**self.alpha *
+                        self.diff_color(j)**self.beta)
             candidates.append(j)
-        summation = sum(vals)
-        # print(summation)
-        for v in vals:
-            v = v/summation
-        print(vals)
+        # summation = sum(vals)
+        # for v in range(len(vals)):
+        #     vals[v] = vals[v]/summation
         maxval = max(vals)
         for i in range(len(candidates)):
             if vals[i] >= maxval:
@@ -91,46 +98,41 @@ class Ant:
         self.start = candidate
         return candidate
 
-    def get_phero_trail(self):
+    def get_phero_trail(self):  # gets pheromone trail of a path
         phero_trail = np.zeros((number_of_nodes+1, number_of_nodes+1), float)
         for i in nodes:
             for j in nodes:
+                # if two nodes have the same colour
                 if self.assigned_colors[i] == self.assigned_colors[j]:
                     phero_trail[i, j] = 1
         return phero_trail
 
 
-def graph(path):
+def graph(path):  # constructing graph using txt file
     g = nx.Graph()
     with open(path, "r") as a_file:
         line = a_file.readline().split()
-        number_of_nodes = line[-2]
-        number_of_edges = line[-1]
         for line in a_file:
             stripped_line = line.strip().split()
-            # print(stripped_line)
             g.add_edge(int(stripped_line[1]), int(stripped_line[2]))
     return g
 
 
-def draw_graph(g, col_val):
+def draw_graph(g, col_val):  # visualizing the graph and the final colours
     pos = nx.spring_layout(g)
-    values = [col_val.get(node, 'blue') for node in g.nodes()]
-    # with_labels=true is to show the node number in the output graph
+    values = [col_val.get(node, 'green') for node in g.nodes()]
     nx.draw(g, pos, with_labels=True, node_color=values,
             edge_color='black', width=1, alpha=0.7)
 
 
-def init_colors(g):
+def initialize_colors(g):  # initially number of colours = number of nodes
     colors = []
-    grundy = len(nx.degree_histogram(g))
-    for c in range(grundy):
+    for c in range(number_of_nodes):
         colors.append(c)
-    # print(grundy, len(g.nodes()))
     return colors
 
 
-def adjacency_matrix(g):
+def adjacency_matrix(g):  # constructing the adjacency matrix
     adj_matrix = np.zeros((number_of_nodes+1, number_of_nodes+1), int)
     for node in nodes:
         for adj_node in g.neighbors(node):
@@ -138,15 +140,16 @@ def adjacency_matrix(g):
     return adj_matrix
 
 
-def init_pheros(g):
+def init_pheros(g):  # initialising pheromone matrix
     phero_mat = np.ones((number_of_nodes+1, number_of_nodes+1), float)
     for node in g:
         for neighbor in g.neighbors(node):
+            # if nodes are neighbours, pheremone value = 0
             phero_mat[node][neighbor] = 0
     return phero_mat
 
 
-def colony():
+def colony():  # initilizing colony of ants
     ants = []
     for i in range(number_of_ants):
         a = Ant(0.8, 0.8)
@@ -154,26 +157,27 @@ def colony():
     return ants
 
 
-def evaporation():
+def evaporation():  # applying evaporation on the pheremone matrix
     for node in nodes:
         for neighbor in nodes:
             phero_mat[node, neighbor] = phero_mat[node, neighbor]*(1-rho)
 
 
-def getbest():  # check this
+def getbest():  # returns the best solution yet
     global phero_mat
-    best = 0
-    elite = None
+    best = 0  # least number of colours used
+    elite = None  # best solution
     for ant in ants:
-        if(best == 0):
+        if best == 0:
             best = ant.colors_used
             elite = ant
-        elif ant.colors_used < best:
+        elif ant.colors_used < best:  # if a new best appears
             best = ant.colors_used
             elite = ant
-    # update pheromones mat
+    # update pheromones matrix
+    # get pheremone trail of the best solution
     elite_phero = elite.get_phero_trail()
-    phero_mat = phero_mat + elite_phero
+    phero_mat = phero_mat + elite_phero  # update pheromone matrix by adding the two
     return elite.colors_used, elite.assigned_colors
 
 
@@ -205,7 +209,7 @@ def algo(gr, numberants, iterations, alpha, beta, evap):
     nodes.sort()
 
     adjacency_mat = adjacency_matrix(gr)
-    colors = init_colors(gr)
+    colors = initialize_colors(gr)
     phero_mat = init_pheros(gr)
     number_of_nodes = nx.number_of_nodes(gr)
 
@@ -219,21 +223,21 @@ def algo(gr, numberants, iterations, alpha, beta, evap):
             colors_in_final = best_colors
             solution = best_sol
             iterations_needed = i+1
-        elif(best_colors < colors_in_final):
+        elif(best_colors < colors_in_final):  # a new better solution appears swap it with current best
             colors_in_final = best_colors
             solution = best_sol
             iterations_needed = i+1
     return colors_in_final, solution, iterations_needed
 
 
-g = graph('t.txt')
+g = graph('gcol1.txt')
 number_of_nodes = 0
 nodes = []
 number_of_ants = 0
 rho = 0.8
 phero_mat = np.ones((number_of_nodes, number_of_nodes), float)
 adjacency_mat = np.zeros((number_of_nodes, number_of_nodes), float)
-final_costs, final_solution, iterations_needed = algo(g, 20, 5, 0.8, 0.8, 0.8)
+final_costs, final_solution, iterations_needed = algo(g, 20, 20, 0.8, 0.8, 0.8)
 print(final_costs, final_solution, iterations_needed)
 draw_graph(g, final_solution)
 plt.show()
